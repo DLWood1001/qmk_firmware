b70d10dbbbe1a61fc786e50e85cd0983231a4230
Daniel Wood <daniel.l.wood@gmail.com>
2020-11-22 13:13:01 -0500

    Added Kyria Keymap.

---
+++ keyboards/kyria/keymaps/dlwood/rules.mk
@@ -0,0 +1,14 @@
+OLED_DRIVER_ENABLE = yes # Enables the use of OLED displays
+ENCODER_ENABLE = no # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+WEBUSB_ENABLE = no
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = yes
+NKRO_ENABLE = yes
+BOOTMAGIC_ENABLE = no
+ONESHOT_ENABLE = no
